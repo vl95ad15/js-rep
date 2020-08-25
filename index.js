36e9c29e31e0879785a7dfc5511c5197b6a7f20a
Vlad <martsul.vlad@gmail.com>
2020-08-26 00:01:42 +0300

    Homework (Table)

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
-// Task 1.0 (Заполнить массив 20 числами которые делятся на 3, или на 5, или на 15 и вывести его на экран (цикл for))
+// Task 2.0 (Вывести матрицу таблицы умножения)
 
-const a = [];
+let table = [];
 
-for (let i = 1; i <= 100; i += 1) {
-    if (i % 3 == 0) {
-        a.push(i);
+for (let i = 1; i <= 10; i++) {
+    let row = [];
+    for (let j = 1; j <= 10; j++) {
+        row.push(i * j);
     }
-    if (i % 5 == 0) {
-        a.push(i);
-    }
-    if (i % 15 == 0) {
-        a.push(i);
-    }
+    table.push(row);
 }
 
-for (let i = 0; i < 20; i += 1) {
-    console.log(`a[${i}] => ${a[i]}`);
+for (let i = 0; i < table.length; i++) {
+    let row = table[i];
+    for (let j = 0; j < row.length; j++) {
+        let value = row[j];
+        process.stdout.write(value.toString() + '\t');
+    }
+
+    console.log()
 }
-
-// Task 1.1 (Посчитать произведение всех элементов находящихся в каждой третьей позиции)
-
-const b = a[2] * a[5] * a[8] * a[11] * a[14] * a[17];
-
-console.log(b)
-
-// Task 1.2 (Посчитать среднее арифметическое всех чисел)
-
-let sum = 0;
-
-for (let i = 0; i < a.length; i += 1) {
-    sum += a[i];
-}
-
-let avarage = sum / a.length;
-console.log(avarage);
-
-// Task 1.3 (Найти максимальное и минимальное число)
-
-console.log(Math.max.apply(null, a));
-console.log(Math.min.apply(null, a));
