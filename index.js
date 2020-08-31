cfcfd436e0d086bf36991fdee5d33136bf74a1ea
Vlad <martsul.vlad@gmail.com>
2020-08-31 23:23:56 +0300

    Add functions (Part 1)

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-// Task 2.0 (Вывести матрицу таблицы умножения)
-
-let table = [];
-
-for (let i = 1; i <= 10; i++) {
-    let row = [];
-    for (let j = 1; j <= 10; j++) {
-        row.push(i * j);
-    }
-    table.push(row);
+function distance(x1, y1, x2, y2) {
+    const dist = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    return dist;
 }
 
-for (let i = 0; i < table.length; i++) {
-    let row = table[i];
-    for (let j = 0; j < row.length; j++) {
-        let value = row[j];
-        process.stdout.write(value.toString() + '\t');
-    }
+console.log(distance(15, 10, 25, 15));
 
-    console.log()
+console.log("/////////////////////////////////////////////////");
+
+function theorem(a, b, c) {
+    if (Math.pow(c, 2) === Math.pow(a, 2) + Math.pow(b, 2)) {
+        return "Числа удовлетворяют теореме Пифагора";
+    } else {
+        return "Числа НЕудовлетворяют теореме Пифагора";
+    }
 }
+
+console.log(theorem(3, 4, 5));
+
+console.log("/////////////////////////////////////////////////");
+
+function triangle(a1, b1, c1) {
+    const check1 = a1 + b1 > c1;
+    const check2 = b1 + c1 > a1;
+    const check3 = c1 + a1 > b1;
+
+    const answer = check1 && check2 && check3;
+
+    const action = answer ? "можно" : "нельзя";
+
+    return `На сторонах с длинами (${a1}, ${b1}, ${c1}) ${action} построить треугольник.`;
+}
+
+console.log(triangle(12, 13, 14));
