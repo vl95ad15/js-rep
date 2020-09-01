172404cb7daa41f0a69031ce732b03eb4e39276c
Vlad <martsul.vlad@gmail.com>
2020-09-01 21:48:34 +0300

    Update hw1

--- hw1.js
+++ hw1.js
@@ -8,27 +8,25 @@ console.log(distance(15, 10, 25, 15));
 console.log("/////////////////////////////////////////////////");
 
 function theorem(a, b, c) {
-    if (Math.pow(c, 2) === Math.pow(a, 2) + Math.pow(b, 2)) {
-        return "Числа удовлетворяют теореме Пифагора";
-    } else {
-        return "Числа НЕудовлетворяют теореме Пифагора";
-    }
+    const p = (Math.pow(c, 2) === Math.pow(a, 2) + Math.pow(b, 2));
+    return p;
 }
 
-console.log(theorem(3, 4, 5));
+const result = theorem(3, 4, 5);
+
+const answer = result ? "удовлетворяют" : "не удовлетворяют";
+
+console.log(`Числа ${answer} теореме Пифагора`);
 
 console.log("/////////////////////////////////////////////////");
 
 function triangle(a1, b1, c1) {
-    const check1 = a1 + b1 > c1;
-    const check2 = b1 + c1 > a1;
-    const check3 = c1 + a1 > b1;
-
-    const answer = check1 && check2 && check3;
-
-    const action = answer ? "можно" : "нельзя";
-
-    return `На сторонах с длинами (${a1}, ${b1}, ${c1}) ${action} построить треугольник.`;
+    const check = (a1 + b1 > c1) && (b1 + c1 > a1) && (c1 + a1 > b1);
+    return check;
 }
 
-console.log(triangle(12, 13, 14));
+const res = triangle(12, 13, 14);
+
+const action = res ? "можно" : "нельзя";
+
+console.log(`Итог: ${action} построить треугольник.`);
